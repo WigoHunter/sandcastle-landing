c16e654bcdf27a833c037831e4388fb4a298de84
Kai-Chun Hsu <mattandkevin1060@gmail.com>
2020-02-13 17:52:20 -0500

    initial upload

--- src/App.js
+++ src/App.js
@@ -1,24 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [email, setEmail] = useState("");
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="landing" />
+        <div className="titles">
+          <h2>A New Kind of Business Card</h2>
+          <p>Enabled by NFC Technology - just like Apple Cards</p>
+
+          <div id="mc_embed_signup">
+            <form
+              action="https://gmail.us4.list-manage.com/subscribe/post?u=28dfefda59d885f2b409f916a&amp;id=c05b220e77"
+              method="post"
+              id="mc-embedded-subscribe-form"
+              name="mc-embedded-subscribe-form"
+              class="validate"
+              target="_blank"
+              novalidate
+            >
+              <div id="mc_embed_signup_scroll">
+                <input
+                  type="email"
+                  value={email}
+                  name="EMAIL"
+                  onChange={e => setEmail(e.target.value)}
+                  class="email"
+                  id="mce-EMAIL"
+                  placeholder="email address"
+                  required
+                />
+                <input
+                  type="submit"
+                  value="Subscribe"
+                  name="subscribe"
+                  id="mc-embedded-subscribe"
+                  class="button btn-submit"
+                />
+              </div>
+            </form>
+          </div>
+        </div>
       </header>
+
+      <div className="info">
+        <h3>Instant Connection.</h3>
+        <p>
+          without requiring people to take out their phones, and have
+          conversation being interrupted.
+        </p>
+      </div>
+
+      <div className="footer">
+        <p>Made by a group of master students from Cornell Tech.</p>
+      </div>
     </div>
   );
 }
